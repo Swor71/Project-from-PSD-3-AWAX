6218a08da56176fe9561ee1029cdc2b3079494be
Marcin Swornowski <swor71@gmail.com>
2018-05-24 21:32:30 +0200

    Add all stages of RWD

--- js/index.js
+++ js/index.js
@@ -21,14 +21,13 @@ function initMap() {
     })
 
     $(".carousel").slick({
-      autoplay: false,
+      autoplay: true,
       slidesToShow: 1,
       slidesToScroll: 1,
       arrows: false,
       dots: true,
       autoplaySpeed: 3500
     });
-
 
     let options = {
       ovalWidth: 450,
@@ -43,34 +42,27 @@ function initMap() {
 
     const carousel = $('.profile-carousel').CircularCarousel(options);
 
-    /* Fires when an item is about to start it's activate animation */
     carousel.on('itemBeforeActive', function (e, item) {
       $(item).css("box-shadow", "0 0 30px #b88b58");
     });
 
-    /* Fires after an item finishes it's activate animation */
     carousel.on('itemActive', function (e, item) {
       $(item).css("box-shadow", "0 0 30px #b88b58");
     });
 
-    /* Fires when an active item starts it's de-activate animation */
     carousel.on('itemBeforeDeactivate', function (e, item) {
       $(item).css("box-shadow", "0 0 30px #b88b58");
     })
 
-    /* Fires after an active item has finished it's de-activate animation */
     carousel.on('itemAfterDeactivate', function (e, item) {
       $(item).css('box-shadow', '');
     })
 
-
-    /* Previous button */
     $('.controls .previous').click(function (e) {
       carousel.cycleActive('previous');
       e.preventDefault();
     });
 
-    /* Next button */
     $('.controls .next').click(function (e) {
       carousel.cycleActive('next');
       e.preventDefault();
