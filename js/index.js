e5b6cadb2ba2b00e9a4d7f8268cb94cae69af5e6
Marcin Swornowski <swor71@gmail.com>
2018-05-26 13:07:55 +0200

    Replace hero carousel with a new one, narrowing to one carousel lib for the whole page

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 function initMap() {
   const pos = { lat: -37.817211, lng: 144.955924 };
   const map = new google.maps.Map(document.getElementById("map"), {
@@ -10,96 +11,60 @@ function initMap() {
   });
 }
 
-(function () {
-
-  $(document).ready(function () {
-
-    var clientSwiper = new Swiper(".client-swiper-container", {
-      effect: "flip",
-      grabCursor: true,
-      loop: true,
-      speed: 400,
-      pagination: {
-        el: ".swiper-pagination"
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-      }
-    });
-
-    var teamSwiper = new Swiper('.team-swiper-container', {
-      effect: 'coverflow',
-      grabCursor: true,
-      centeredSlides: false,
-      slidesPerView: 'auto',
-      initialSlide: 1,
-      coverflowEffect: {
-        rotate: 50,
-        stretch: 0,
-        depth: 100,
-        modifier: 1,
-        slideShadows: false,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-      },
-    });
-
-    $('#menu-icon').click(function () {
-      $("#navbar-list").toggle(400, function () {
-        $(this).toggleClass('toggle-menu').css('display', '');
-      });
-    })
-
-    $(".carousel").slick({
-      autoplay: true,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: false,
-      dots: true,
-      autoplaySpeed: 3500
-    });
-
-    let options = {
-      ovalWidth: 450,
-      ovalHeight: 25,
-      offsetX: 75,
-      offsetY: 320,
-      angle: 0,
-      activeItem: 0,
-      duration: 350,
-      className: 'profile-item'
+$(document).ready(function () {
+  
+  var heroSwiper = new Swiper(".hero-swiper-container", {
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+      delay: 4000
+    },
+    speed: 800,
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true
+  });
+  
+  var teamSwiper = new Swiper(".team-swiper-container", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: false,
+    slidesPerView: "auto",
+    initialSlide: 1,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: false
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
     }
-
-    const carousel = $('.profile-carousel').CircularCarousel(options);
-
-    carousel.on('itemBeforeActive', function (e, item) {
-      $(item).css("box-shadow", "0 0 30px #b88b58");
-    });
-
-    carousel.on('itemActive', function (e, item) {
-      $(item).css("box-shadow", "0 0 30px #b88b58");
-    });
-
-    carousel.on('itemBeforeDeactivate', function (e, item) {
-      $(item).css("box-shadow", "0 0 30px #b88b58");
-    })
-
-    carousel.on('itemAfterDeactivate', function (e, item) {
-      $(item).css('box-shadow', '');
-    })
-
-    $('.controls .previous').click(function (e) {
-      carousel.cycleActive('previous');
-      e.preventDefault();
-    });
-
-    $('.controls .next').click(function (e) {
-      carousel.cycleActive('next');
-      e.preventDefault();
-    });
-
+  });
+  
+  var clientSwiper = new Swiper(".client-swiper-container", {
+    effect: "flip",
+    grabCursor: true,
+    loop: true,
+    speed: 400,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    }
   });
 
-})();
+  $('#menu-icon').click(function () {
+    $("#navbar-list").toggle(400, function () {
+      $(this).toggleClass('toggle-menu').css('display', '');
+    });
+  })
+
+});
